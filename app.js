f8e59c5870d03ac7bef0adde9a8f7125d7878d34
TayyabKhan1221 <Tayyabkhano603@gmail.com>
2024-04-11 11:41:56 +0500

    assim done

---
+++ app.js
@@ -0,0 +1,15 @@
+let inputForm = document.querySelector("#tableForm");
+let input = document.querySelector("#inputID");
+let table = document.querySelector(".time-table");
+
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let tableOf = input.value;
+
+  table.innerHTML = "";
+  for (let i = 1; i <= 12; i++) {
+    const message = `${tableOf} X ${i} = ${tableOf * i}`;
+    table.innerHTML += `${message} <br>`;
+  }
+});
